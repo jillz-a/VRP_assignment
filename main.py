4f087c998896ba6d9e2c2ee0ea2563637b5cb544
jnaalders <jaime.aalders@gmail.com>
2022-04-28 15:43:34 +0200

    Lil setup

--- main.py
+++ main.py
@@ -5,3 +5,92 @@
 import numpy as np
 import pandas as pd
 from gurobipy import Model,GRB,LinExpr
+import time
+
+
+
+
+#Define constants
+
+n_nodes = 10
+n_vehicles = 10
+
+
+
+#Define indices list
+n = list(range(n_nodes))
+k = list(range(n_vehicles))
+
+startTimeSetUp = time.time()
+model = Model()
+
+
+#################
+### DEFINE VARIABLES ###
+#################
+
+x ={}
+
+
+model.update()
+#################
+### DEFINE OBJECTIVE FUNCTION ###
+#################
+
+
+obj        = LinExpr() 
+            
+
+model.setObjective(obj,GRB.MINIMIZE)
+model.update()
+model.write('model_formulation.lp')    
+
+###################
+### DEFINE CONSTRAINTS ###
+###################
+
+           
+#Each vehicle must leave the depot
+
+
+
+
+
+#Each vehicle must return to the depot
+
+
+
+
+
+
+
+#Each customer must be visited by a vehicle
+
+
+
+
+#If a vehicle visits a custome, then the same vehicle must leave that customer
+
+
+
+
+#Capacity contraints
+
+
+
+
+
+#Time Window constraints
+
+
+
+
+#(backhauls)
+
+
+
+
+
+
+model.optimize()
+endTime   = time.time()
