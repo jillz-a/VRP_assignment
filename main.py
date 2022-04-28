e47b597c6e6234ae5f1f6253894f32c4b5441dd3
jilles <jilles@ziggo.nl>
2022-04-28 13:46:44 +0200

    little imports

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 #Operations Optimization assignment
-#Vehicle routing problem
-#By: Jaime Aalders, Mitchell de Keijzer
+#Vehicle routing problem applied to baggage transportation from and to aircraft
+#By: Jaime Aalders, Mitchell de Keijzer, Jilles Andringa
 
+import numpy as np
+import pandas as pd
+from gurobipy import Model,GRB,LinExpr
