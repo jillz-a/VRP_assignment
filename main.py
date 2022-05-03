87eb18af7dc1b90b2f157f5bc06e5468f622d529
jilles <jilles@ziggo.nl>
2022-05-03 16:40:36 +0200

    eerste toevoegingen van algemene model inclusief visualisatie

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 #Operations Optimization assignment
 #Vehicle routing problem applied to baggage transportation from and to aircraft
 #By: Jaime Aalders, Mitchell de Keijzer, Jilles Andringa
-
-import numpy as np
 import pandas as pd
-from gurobipy import Model,GRB,LinExpr
+from gurobipy import Model,GRB,LinExpr, quicksum
 import time
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
-
+#import data
+dist = pd.read_excel('nodes_loc.xlsx', sheet_name='dist', header=None) #distances between the nodes
+pos = pd.read_excel('nodes_loc.xlsx', sheet_name='loc')
 
 #Define constants
 
-n_nodes = 10
-
-
-
+n_vehicles = 1 #number of bagage vehicles
 
 #Define lists
-n = list(range(n_nodes))
+
+n_nodes = len(dist[0]) #number of nodes including central node 0
 
 
 startTimeSetUp = time.time()
@@ -30,8 +30,12 @@ model = Model()
 #################
 
 x = {}
-k = {}
 
+for n1 in range(n_nodes):
+    for n2 in range(n_nodes):
+        if n1 != n2:
+            for k in range(n_vehicles):
+                x[n1,n2,k] = model.addVar(vtype=GRB.BINARY, name= "x[%d, %d, %d]"%(n1,n2,k))
 
 model.update()
 #################
@@ -39,7 +43,13 @@ model.update()
 #################
 
 
-obj        = LinExpr() 
+obj = LinExpr()
+
+for n1 in range(n_nodes):
+    for n2 in range(n_nodes):
+        if n1 != n2:
+            for k in range(n_vehicles):
+                obj += x[n1,n2,k] * dist[n1][n2]
             
 
 model.setObjective(obj,GRB.MINIMIZE)
@@ -52,14 +62,14 @@ model.setObjective(obj,GRB.MINIMIZE)
            
 #Each vehicle must leave the depot
 
-
-
+for k in range(n_vehicles):
+    model.addConstr(quicksum(x[0,n2,k] for n2 in range(1,n_nodes)), GRB.EQUAL, 1)
 
 
 #Each vehicle must return to the depot
 
-
-
+for k in range(n_vehicles):
+    model.addConstr(quicksum(x[n1,0,k] for n1 in range(1,n_nodes)), GRB.EQUAL, 1)
 
 
 
@@ -67,11 +77,16 @@ model.setObjective(obj,GRB.MINIMIZE)
 #Each customer must be visited by a vehicle
 
 
+for n2 in range(n_nodes):
+    model.addConstr(quicksum(x[n1,n2,k] for k in range(n_vehicles) for n1 in range(n_nodes) if n1 != n2), GRB.EQUAL, 1)
 
 
-#If a vehicle visits a custome, then the same vehicle must leave that customer
 
+#If a vehicle visits a customer, then the same vehicle must leave that customer
 
+for n1 in range(n_nodes):
+    for k in range(n_vehicles):
+            model.addConstr(quicksum(x[n1,n2,k] for n2 in range(n_nodes) if n2 != n1), GRB.EQUAL, quicksum(x[n2,n1,k] for n2 in range(n_nodes) if n2 != n1))
 
 
 #Capacity contraints
@@ -95,3 +110,20 @@ model.update()
 model.write('model_formulation.lp')   
 model.optimize()
 endTime   = time.time()
+
+#visualize
+G = nx.DiGraph()
+
+for node in range(n_nodes):
+    G.add_node(node, pos = (pos['x'][node], pos['y'][node]))
+
+for n1 in range(n_nodes):
+    for n2 in range(n_nodes):
+        if n1 != n2:
+            for k in range(n_vehicles):
+                if x[(n1,n2,k)].X == 1:
+                    G.add_edge(n1,n2, arrowstyle = "->")
+
+pos=nx.get_node_attributes(G,'pos')
+nx.draw(G, pos, with_labels= True)
+plt.show()
