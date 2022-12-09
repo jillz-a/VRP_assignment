8d0e403485b210c0c0a2b1ed231d63c96845b69d
jnaalders <jaime.aalders@gmail.com>
2022-12-09 15:40:58 +0100

    Veel werk

---
+++ NewProject/functions.py
@@ -0,0 +1,218 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  9 13:12:40 2022
+
+@author: jaime
+"""
+
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.io as pio
+pio.renderers.default='browser'
+
+def spherical_dist_rad(phi1, phi2, lambda1, lambda2, R=6367000):
+    """
+    Great circle distance in meters from coordinates in radians
+
+    Parameters
+    ----------
+    phi1 : float
+        Starting point latitude in radians north.
+    phi2 : float
+        End point latitude in radians north.
+    lambda1 : float
+        Starting point longitude in radians east.
+    lambda2 : float
+        End point longitude in radians east.
+    R : float, optional
+        Radius of the Earth in meters. The default is 6367000.
+
+    Returns
+    -------
+    d : float
+        Great circle distance in meters.
+
+    """
+    d = R * 2 * np.arcsin(np.sqrt(
+        np.square(np.sin((phi2 - phi1) / 2))
+        + np.cos(phi1) * np.cos(phi2) * np.square(np.sin((lambda2 - lambda1)
+                                                         / 2))))
+    
+    return d
+
+def retrieve_apts():
+    
+    df_apts = pd.read_csv('airports.csv')
+    df = pd.read_csv('KLC_flightlist_20190901.csv')
+    #df_apts.loc[df['iata'] == 'AMS']
+    route_lst = []
+    origin_lst = []
+    destination_lst = []
+    lat_lst1 = []
+    lon_lst1 = []
+    lat_lst2 =[]
+    lon_lst2 =[]
+    source_to_dest = []
+    
+    for flight_idx in range(len(df['callsign'])):
+        origin_apt = df['origin_apt'][flight_idx]
+        destination_apt = df['destination_apt'][flight_idx]
+        
+        origin = df_apts.loc[df_apts['iata'] == origin_apt]
+        lat_o = origin['lat'].values[0]
+        lon_o = origin['lon'].values[0]
+
+        
+        destination = df_apts.loc[df_apts['iata'] == destination_apt]
+
+        lat_d = destination['lat'].values[0]
+        lon_d = destination['lon'].values[0]
+         
+        
+        route_dist = spherical_dist_rad(lat_o, lat_d, lon_o, lon_d, R=6367000)
+        
+        origin_lst.append(origin_apt)
+        destination_lst.append(destination_apt)
+        route_lst.append(route_dist)
+        lat_lst1.append(lat_o)
+        lon_lst1.append(lon_o)
+        lat_lst2.append(lat_d)
+        lon_lst2.append(lon_d)
+        source_to_dest.append([lat_o, lat_d, lon_o, lon_d, origin_apt,destination_apt])
+        
+    
+    d = {'origin': origin_lst, 'destination': destination_lst, 'route': route_lst, 'lat_origin':lat_lst1, 'lon_origin': lon_lst1, 'lat_destination':lat_lst2, 'lon_destination': lon_lst2}
+    exp_df = pd.DataFrame(data=d)
+    exp_df.to_csv('routes.csv')
+    
+    
+    
+    #import pdb;pdb.set_trace()
+    
+    unique_lst = []
+    pos_lat = []
+    pos_lon = []
+    
+    
+    for j in exp_df['origin'].unique():
+        unique_lst.append(j)
+    
+    for i in exp_df['destination'].unique():
+        if i not in unique_lst:
+            unique_lst.append(i)
+            
+    
+    for airport in unique_lst:
+        apt = df_apts.loc[df_apts['iata'] == airport]
+        lat = apt['lat'].values[0]
+        lon = apt['lon'].values[0]
+        
+        pos_lat.append(lat)
+        pos_lon.append(lon)
+    
+    #import pdb; pdb.set_trace()
+    pos_lat1 = [x for _,x in sorted(zip(unique_lst,pos_lat))]
+    pos_lon1 = [x for _,x in sorted(zip(unique_lst,pos_lon))]
+    unique_lst.sort()
+        
+    #d2 = {'airport' : unique_lst, 'lat': pos_lat1, 'lon': pos_lon1}
+    d2 = {'x': pos_lon1, 'y': pos_lat1}
+    exp_df2 = pd.DataFrame(data=d2)
+    exp_df2.to_csv('nodes_loc.csv')
+    
+    
+    mat = np.zeros((len(unique_lst),len(unique_lst)))
+    
+    for apt1 in range(len(unique_lst)):
+        for apt2 in range(len(unique_lst)):
+            
+            origin = df_apts.loc[df_apts['iata'] == unique_lst[apt1]]
+            lat_o = origin['lat'].values[0]
+            lon_o = origin['lon'].values[0]
+
+            
+            destination = df_apts.loc[df_apts['iata'] == unique_lst[apt2]]
+
+            lat_d = destination['lat'].values[0]
+            lon_d = destination['lon'].values[0]
+             
+            
+            route_dist = spherical_dist_rad(lat_o, lat_d, lon_o, lon_d, R=6367000)
+            
+            mat[apt1][apt2] = route_dist
+    
+    exp_df3 = pd.DataFrame(mat)
+    exp_df3.to_csv('dist.csv')
+    
+    # Uncomment to Visualize
+    
+# =============================================================================
+# 
+#     
+#     
+#     fig = go.Figure()
+#     lons =[]
+#     lats =[]
+#     dests = []
+#     
+#     # Loop through each flight entry
+#     for slat, dlat, slon, dlon, origin, dest in source_to_dest:
+#     
+#         fig.add_trace(go.Scattergeo(
+#                             lat=[slat, dlat],
+#                             lon=[slon, dlon],
+#                             mode='lines',
+#                             line=dict(width=1, color= 'red')
+#     
+#                             ))
+#         #lons.append(dlon)
+#         #lats.append(dlat)
+#         
+#         if origin == 'AMS':
+#             airport = dest
+#             lons.append(dlon)
+#             lats.append(dlat)
+#         else:
+#             airport = origin
+#             lons.append(slon)
+#             lats.append(slat)
+#         dests.append(airport)
+#         
+#     
+#     
+#     fig.add_trace(
+#         go.Scattergeo(
+#                     lon = lons,
+#                     lat = lats,
+#                     hoverinfo = 'text',
+#                     text = dests,
+#                     mode = 'markers+text',
+#                     textposition="top center",
+#                     marker = dict(size = 1, color = 'rgb(0,0,0)', opacity = .5)))
+#     
+#     
+#     fig.update_layout(title_text='Connection Map Depicting Flights for Airline Network',
+#                       height=700, width=900,
+#                       margin={"t": 0, "b": 0, "l": 0, "r": 0, "pad": 0},
+#                       showlegend=False,
+#                       geo=dict(showland=True, landcolor='white', countrycolor='lightgrey', bgcolor="lightgrey", resolution=50))
+#     # =============================================================================
+#     # 
+#     # fig.add_trace(
+#     #     go.Scattergeo(
+#     #                 lon=airports_lon,
+#     #                 lat=airports_lat,
+#     #                 hoverinfo='text',
+#     #                 text=airports,
+#     #                 mode='markers+text',
+#     #                 textposition="top center",
+#     #                 marker=dict(size=3, color='rgb(0,0,0)', opacity=1)))
+#     # =============================================================================
+#     
+#     fig.show()
+#             
+#         
+# =============================================================================
+
+
---
+++ NewProject/main.py
@@ -0,0 +1,204 @@
+#Operations Optimization assignment
+#Vehicle routing problem for train scheduling of all KLM City hopper flights
+#By: Jaime Aalders, Mitchell de Keijzer, Jilles Andringa
+import pandas as pd
+from gurobipy import Model,GRB,LinExpr, quicksum
+import time
+import networkx as nx
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly.io as pio
+
+
+#import data
+dist = pd.read_excel('nodes_loc.xlsx', sheet_name='dist', header=None) #distances between the nodes
+pos = pd.read_excel('nodes_loc.xlsx', sheet_name='loc')
+
+
+
+#Define constants
+
+n_vehicles = 1 #number of trains
+
+#Define lists
+
+n_nodes = len(dist) #number of nodes including central node 0
+
+
+startTimeSetUp = time.time()
+model = Model()
+
+
+#################
+### DEFINE VARIABLES ###
+#################
+
+x = {}
+
+for n1 in range(n_nodes):
+    for n2 in range(n_nodes):
+        if n1 != n2:
+            for k in range(n_vehicles):
+                x[n1,n2,k] = model.addVar(vtype=GRB.BINARY, name= "x[%d, %d, %d]"%(n1,n2,k))
+
+model.update()
+#################
+### DEFINE OBJECTIVE FUNCTION ###
+#################
+
+
+obj = LinExpr()
+
+for n1 in range(n_nodes):
+    for n2 in range(n_nodes):
+        if n1 != n2:
+            for k in range(n_vehicles):
+                obj += x[n1,n2,k] * dist[n1][n2]
+            
+
+model.setObjective(obj,GRB.MINIMIZE)
+ 
+
+###################
+### DEFINE CONSTRAINTS ###
+###################
+
+           
+#Each vehicle must leave the depot
+
+for k in range(n_vehicles):
+    model.addConstr(quicksum(x[0,n2,k] for n2 in range(1,n_nodes)), GRB.EQUAL, 1)
+
+
+#Each vehicle must return to the depot
+
+for k in range(n_vehicles):
+    model.addConstr(quicksum(x[n1,0,k] for n1 in range(1,n_nodes)), GRB.EQUAL, 1)
+
+
+
+
+#Each customer must be visited by a vehicle
+
+
+for n2 in range(n_nodes):
+    model.addConstr(quicksum(x[n1,n2,k] for k in range(n_vehicles) for n1 in range(n_nodes) if n1 != n2), GRB.EQUAL, 1)
+
+
+
+#If a vehicle visits a customer, then the same vehicle must leave that customer
+for k in range(n_vehicles):
+    for n1 in range(n_nodes):
+            model.addConstr(quicksum(x[n1,n2,k] for n2 in range(n_nodes) if n2 != n1), GRB.EQUAL, quicksum(x[n2,n1,k] for n2 in range(n_nodes) if n2 != n1))
+
+#Subtour elimination
+
+
+for k in range(n_vehicles):
+    model.addConstr(quicksum(x[i,j,k] for i in range(1,n_nodes) for j in range(1,n_nodes) if i != j), GRB.LESS_EQUAL, n_nodes -2)
+
+#Capacity contraints
+
+
+
+
+#Time Window constraints
+
+
+
+
+#(backhauls)
+
+
+
+
+
+model.update()
+model.write('model_formulation.lp')   
+model.optimize()
+endTime   = time.time()
+
+#visualize
+
+slat_lst = []
+slon_lst = []
+dlat_lst = []
+dlon_lst = []
+nr_flights = []
+
+for n1 in range(n_nodes):
+    for n2 in range(n_nodes):
+        if n1 != n2:
+            for k in range(n_vehicles):
+                if x[n1,n2,k].X > 0:
+                    #import pdb;pdb.set_trace()
+                    nr_flights.append(k)
+                    slat_lst.append(pos['y'][n1])
+                    dlat_lst.append(pos['y'][n2])
+                    slon_lst.append(pos['x'][n1])
+                    dlon_lst.append(pos['x'][n2])
+
+
+fig = go.Figure()
+source_to_dest = zip(slat_lst, dlat_lst, slon_lst, dlon_lst, nr_flights)
+
+# Loop through each flight entry
+for slat, dlat, slon, dlon, num_flights in source_to_dest:
+
+    fig.add_trace(go.Scattergeo(
+                        lat=[slat, dlat],
+                        lon=[slon, dlon],
+                        mode='lines',
+                        line=dict(width=num_flights + 1, color = 'red')
+                        ))
+
+
+fig.update_layout(title_text='Connection Map Depicting Flights for Airline Network',
+                  height=700, width=900,
+                  margin={"t": 0, "b": 0, "l": 0, "r": 0, "pad": 0},
+                  showlegend=False,
+                  geo=dict(showland=True, landcolor='white', countrycolor='grey', bgcolor="lightgrey", resolution=50))
+
+# =============================================================================
+# fig.add_trace(
+#     go.Scattergeo(
+#                 lon=airports_lon,
+#                 lat=airports_lat,
+#                 hoverinfo='text',
+#                 text=airports,
+#                 mode='markers+text',
+#                 textposition="top center",
+#                 marker=dict(size=3, color='rgb(0,0,0)', opacity=1)))
+# 
+# =============================================================================
+fig.show()
+
+
+# =============================================================================
+# G = nx.DiGraph()
+# 
+# for node in range(n_nodes):
+#     G.add_node(node, pos = (pos['x'][node], pos['y'][node]))
+# 
+# for n1 in range(n_nodes):
+#     for n2 in range(n_nodes):
+#         if n1 != n2:
+#             for k in range(n_vehicles):
+#                 if x[(n1,n2,k)].X == 1:
+#                     G.add_edge(n1,n2, arrowstyle = "->")
+# 
+# pos=nx.get_node_attributes(G,'pos')
+# nx.draw(G, pos, with_labels= True)
+# 
+# edges = [(n1,n2) for n1,n2 in G.edges]
+# edge_labels = {}
+# for i in range(len(edges)):
+#     edge_labels[edges[i]] = i
+# 
+# nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+# 
+# plt.show()
+# =============================================================================
+
+
+
