e49316d6602038073c38258653016168b7c22e92
jilles <jilles@ziggo.nl>
2023-02-10 19:07:08 +0100

    variable train capacity

--- NewProject/vehiclerouting.py
+++ NewProject/vehiclerouting.py
@@ -15,6 +15,7 @@ def demand(dem, n):
     return dem['capacity'][n] * dem['freq'][n]
 
 train_capacity = 377
+n_trains = 5
 
 startTimeSetUp = time.time()
 model = Model()
@@ -22,9 +23,9 @@ model = Model()
 #add variables
 x = {}
 for index, row in r_out.iterrows():
-    for a in range(1,4):
-        for b in range(1,4):
-            for c in range(4):
+    for a in range(1, n_trains):
+        for b in range(1, n_trains):
+            for c in range(n_trains):
                 x[1, index, a, b, c] = model.addVar(vtype=GRB.BINARY, name= "x[%d, %d, %d, %d, %d]"%(1, index, a, b, c))
                 x[0, index, a, b, c] = model.addVar(vtype=GRB.BINARY, name= "x[%d, %d, %d, %d, %d]"%(0, index, a, b, c))
 model.update()
@@ -34,9 +35,9 @@ print('Defined all variables', time.time() - startTimeSetUp)
 obj = LinExpr()
 
 for index, row in r_all.iterrows():
-    for a in range(1,4):
-        for b in range(1,4):
-            for c in range(4):
+    for a in range(1, n_trains):
+        for b in range(1, n_trains):
+            for c in range(n_trains):
                 if row[0] == 1:
                     n1, n2, n3 = row[1:4]
                     obj += x[1, index, a, b, c]*(dist[2][n1]*a + dist[n1][n2]*b + dist[n2][n3]*c)
@@ -52,67 +53,67 @@ print('Objective function set', time.time() - startTimeSetUp)
 #for outgoing routes
 for index, row in r_out.iterrows():
     n1, n2, n3 = row[1:4]
-    model.addConstr(quicksum(x[1, index, a, b, c] for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.EQUAL, 1)
+    model.addConstr(quicksum(x[1, index, a, b, c] for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.EQUAL, 1)
 
     if n2 != n3:
-        model.addConstr(quicksum(x[1, index, a, b, c]*a for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.GREATER_EQUAL, np.ceil((demand(dem, n1)+demand(dem, n2)+demand(dem, n3))/train_capacity))
-        model.addConstr(quicksum(x[1, index, a, b, c]*b for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.GREATER_EQUAL, np.ceil((demand(dem, n2)+demand(dem, n3))/train_capacity))
-        model.addConstr(quicksum(x[1, index, a, b, c]*c for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.GREATER_EQUAL, np.ceil((demand(dem, n3))/train_capacity))
+        model.addConstr(quicksum(x[1, index, a, b, c]*a for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.GREATER_EQUAL, np.ceil((demand(dem, n1)+demand(dem, n2)+demand(dem, n3))/train_capacity))
+        model.addConstr(quicksum(x[1, index, a, b, c]*b for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.GREATER_EQUAL, np.ceil((demand(dem, n2)+demand(dem, n3))/train_capacity))
+        model.addConstr(quicksum(x[1, index, a, b, c]*c for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.GREATER_EQUAL, np.ceil((demand(dem, n3))/train_capacity))
 
-        model.addConstr(quicksum(x[1, index, a, b, c]*b for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.LESS_EQUAL, quicksum(x[1, index, a, b, c]*a for a in range(1,4) for b in range(1,4) for c in range(4)))
-        model.addConstr(quicksum(x[1, index, a, b, c]*c for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.LESS_EQUAL, quicksum(x[1, index, a, b, c]*b for a in range(1,4) for b in range(1,4) for c in range(4)))
+        model.addConstr(quicksum(x[1, index, a, b, c]*b for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.LESS_EQUAL, quicksum(x[1, index, a, b, c]*a for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)))
+        model.addConstr(quicksum(x[1, index, a, b, c]*c for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.LESS_EQUAL, quicksum(x[1, index, a, b, c]*b for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)))
 
     if n2 == n3:
-        model.addConstr(quicksum(x[1, index, a, b, 0]*a for a in range(1,4) for b in range(1,4)), GRB.GREATER_EQUAL, np.ceil((demand(dem, n1)+demand(dem, n2))/train_capacity))
-        model.addConstr(quicksum(x[1, index, a, b, 0]*b for a in range(1,4) for b in range(1,4)), GRB.GREATER_EQUAL, np.ceil((demand(dem, n2))/train_capacity))
+        model.addConstr(quicksum(x[1, index, a, b, 0]*a for a in range(1, n_trains) for b in range(1, n_trains)), GRB.GREATER_EQUAL, np.ceil((demand(dem, n1)+demand(dem, n2))/train_capacity))
+        model.addConstr(quicksum(x[1, index, a, b, 0]*b for a in range(1, n_trains) for b in range(1, n_trains)), GRB.GREATER_EQUAL, np.ceil((demand(dem, n2))/train_capacity))
 
-        model.addConstr(quicksum(x[1, index, a, b, c]*b for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.LESS_EQUAL, quicksum(x[1, index, a, b, c]*a for a in range(1,4) for b in range(1,4) for c in range(4)))
+        model.addConstr(quicksum(x[1, index, a, b, c]*b for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.LESS_EQUAL, quicksum(x[1, index, a, b, c]*a for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)))
 
 #for returning routes        
 for index, row in r_ret.iterrows():
     n1, n2, n3 = row[1:4]
-    model.addConstr(quicksum(x[0, index, a, b, c] for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.EQUAL, 1)
+    model.addConstr(quicksum(x[0, index, a, b, c] for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.EQUAL, 1)
     
     #if the route goes out and returns via the same nodes
     if r_out.values.tolist()[index][2] != r_out.values.tolist()[index][3]:
         if [n3, n2, n1] == r_out.values.tolist()[index][1:4]:
-            model.addConstr(quicksum(x[0, index, a, b, c]*a for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.GREATER_EQUAL, np.ceil((demand(Retdem, n3))/train_capacity))
-            model.addConstr(quicksum(x[0, index, a, b, c]*b for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.GREATER_EQUAL, np.ceil((demand(Retdem, n2)+demand(Retdem, n3))/train_capacity))
-            model.addConstr(quicksum(x[0, index, a, b, c]*c for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.GREATER_EQUAL, np.ceil((demand(Retdem, n1)+demand(Retdem, n2)+demand(Retdem, n3))/train_capacity))
+            model.addConstr(quicksum(x[0, index, a, b, c]*a for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.GREATER_EQUAL, np.ceil((demand(Retdem, n3))/train_capacity))
+            model.addConstr(quicksum(x[0, index, a, b, c]*b for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.GREATER_EQUAL, np.ceil((demand(Retdem, n2)+demand(Retdem, n3))/train_capacity))
+            model.addConstr(quicksum(x[0, index, a, b, c]*c for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.GREATER_EQUAL, np.ceil((demand(Retdem, n1)+demand(Retdem, n2)+demand(Retdem, n3))/train_capacity))
 
-            model.addConstr(quicksum(x[0, index, a, b, c]*a for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.EQUAL, quicksum(x[1, index, a, b, c]*c for a in range(1,4) for b in range(1,4) for c in range(4)))
-            model.addConstr(quicksum(x[0, index, a, b, c]*b for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.EQUAL, quicksum(x[1, index, a, b, c]*b for a in range(1,4) for b in range(1,4) for c in range(4)))
-            model.addConstr(quicksum(x[0, index, a, b, c]*c for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.EQUAL, quicksum(x[1, index, a, b, c]*a for a in range(1,4) for b in range(1,4) for c in range(4)))
+            model.addConstr(quicksum(x[0, index, a, b, c]*a for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.EQUAL, quicksum(x[1, index, a, b, c]*c for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)))
+            model.addConstr(quicksum(x[0, index, a, b, c]*b for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.EQUAL, quicksum(x[1, index, a, b, c]*b for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)))
+            model.addConstr(quicksum(x[0, index, a, b, c]*c for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.EQUAL, quicksum(x[1, index, a, b, c]*a for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)))
 
         #if the route returns via different nodes
         else:
-            model.addConstr(quicksum(x[0, index, a, b, c]*a for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.GREATER_EQUAL, np.ceil((demand(Retdem, n1)+demand(Retdem, n2)+demand(Retdem, n3))/train_capacity))
-            model.addConstr(quicksum(x[0, index, a, b, c]*b for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.GREATER_EQUAL, np.ceil((demand(Retdem, n1)+demand(Retdem, n2)+demand(Retdem, n3))/train_capacity))
-            model.addConstr(quicksum(x[0, index, a, b, c]*c for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.GREATER_EQUAL, np.ceil((demand(Retdem, n1)+demand(Retdem, n2)+demand(Retdem, n3))/train_capacity))
+            model.addConstr(quicksum(x[0, index, a, b, c]*a for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.GREATER_EQUAL, np.ceil((demand(Retdem, n1)+demand(Retdem, n2)+demand(Retdem, n3))/train_capacity))
+            model.addConstr(quicksum(x[0, index, a, b, c]*b for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.GREATER_EQUAL, np.ceil((demand(Retdem, n1)+demand(Retdem, n2)+demand(Retdem, n3))/train_capacity))
+            model.addConstr(quicksum(x[0, index, a, b, c]*c for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.GREATER_EQUAL, np.ceil((demand(Retdem, n1)+demand(Retdem, n2)+demand(Retdem, n3))/train_capacity))
 
-            model.addConstr(quicksum(x[1, index, a, b, c]*a for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.GREATER_EQUAL, quicksum(x[0, index, a, b, c]*a for a in range(1,4) for b in range(1,4) for c in range(4)))
-            model.addConstr(quicksum(x[1, index, a, b, c]*b for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.GREATER_EQUAL, quicksum(x[0, index, a, b, c]*a for a in range(1,4) for b in range(1,4) for c in range(4)))
-            model.addConstr(quicksum(x[1, index, a, b, c]*c for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.GREATER_EQUAL, quicksum(x[0, index, a, b, c]*a for a in range(1,4) for b in range(1,4) for c in range(4)))
+            model.addConstr(quicksum(x[1, index, a, b, c]*a for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.GREATER_EQUAL, quicksum(x[0, index, a, b, c]*a for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)))
+            model.addConstr(quicksum(x[1, index, a, b, c]*b for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.GREATER_EQUAL, quicksum(x[0, index, a, b, c]*a for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)))
+            model.addConstr(quicksum(x[1, index, a, b, c]*c for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.GREATER_EQUAL, quicksum(x[0, index, a, b, c]*a for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)))
 
     else:
         #returns via same nodes
         if [n3, n2, n1] == r_out.values.tolist()[index][1:4]:
-            model.addConstr(quicksum(x[0, index, a, b, c]*a for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.GREATER_EQUAL, np.ceil((demand(Retdem, n3))/train_capacity))
-            model.addConstr(quicksum(x[0, index, a, b, c]*b for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.GREATER_EQUAL, np.ceil((demand(Retdem, n2)+demand(Retdem, n3))/train_capacity))
-            model.addConstr(quicksum(x[0, index, a, b, c]*c for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.GREATER_EQUAL, np.ceil((demand(Retdem, n1)+demand(Retdem, n2)+demand(Retdem, n3))/train_capacity))
+            model.addConstr(quicksum(x[0, index, a, b, c]*a for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.GREATER_EQUAL, np.ceil((demand(Retdem, n3))/train_capacity))
+            model.addConstr(quicksum(x[0, index, a, b, c]*b for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.GREATER_EQUAL, np.ceil((demand(Retdem, n2)+demand(Retdem, n3))/train_capacity))
+            model.addConstr(quicksum(x[0, index, a, b, c]*c for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.GREATER_EQUAL, np.ceil((demand(Retdem, n1)+demand(Retdem, n2)+demand(Retdem, n3))/train_capacity))
 
-            model.addConstr(quicksum(x[0, index, a, b, c]*a for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.EQUAL, quicksum(x[1, index, a, b, c]*a for a in range(1,4) for b in range(1,4) for c in range(4)))
-            model.addConstr(quicksum(x[0, index, a, b, c]*b for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.EQUAL, quicksum(x[1, index, a, b, c]*b for a in range(1,4) for b in range(1,4) for c in range(4)))
-            model.addConstr(quicksum(x[0, index, a, b, c]*c for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.EQUAL, quicksum(x[1, index, a, b, c]*c for a in range(1,4) for b in range(1,4) for c in range(4)))
+            model.addConstr(quicksum(x[0, index, a, b, c]*a for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.EQUAL, quicksum(x[1, index, a, b, c]*a for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)))
+            model.addConstr(quicksum(x[0, index, a, b, c]*b for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.EQUAL, quicksum(x[1, index, a, b, c]*b for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)))
+            model.addConstr(quicksum(x[0, index, a, b, c]*c for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.EQUAL, quicksum(x[1, index, a, b, c]*c for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)))
 
         #if the route returns via different nodes
         else:
-            model.addConstr(quicksum(x[0, index, a, b, c]*a for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.GREATER_EQUAL, np.ceil((demand(Retdem, n1)+demand(Retdem, n2)+demand(Retdem, n3))/train_capacity))
-            model.addConstr(quicksum(x[0, index, a, b, c]*b for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.GREATER_EQUAL, np.ceil((demand(Retdem, n1)+demand(Retdem, n2)+demand(Retdem, n3))/train_capacity))
-            model.addConstr(quicksum(x[0, index, a, b, c]*c for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.GREATER_EQUAL, np.ceil((demand(Retdem, n1)+demand(Retdem, n2)+demand(Retdem, n3))/train_capacity))
+            model.addConstr(quicksum(x[0, index, a, b, c]*a for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.GREATER_EQUAL, np.ceil((demand(Retdem, n1)+demand(Retdem, n2)+demand(Retdem, n3))/train_capacity))
+            model.addConstr(quicksum(x[0, index, a, b, c]*b for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.GREATER_EQUAL, np.ceil((demand(Retdem, n1)+demand(Retdem, n2)+demand(Retdem, n3))/train_capacity))
+            model.addConstr(quicksum(x[0, index, a, b, c]*c for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.GREATER_EQUAL, np.ceil((demand(Retdem, n1)+demand(Retdem, n2)+demand(Retdem, n3))/train_capacity))
 
-            model.addConstr(quicksum(x[1, index, a, b, c]*a for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.GREATER_EQUAL, quicksum(x[0, index, a, b, c]*a for a in range(1,4) for b in range(1,4) for c in range(4)))
-            model.addConstr(quicksum(x[1, index, a, b, c]*b for a in range(1,4) for b in range(1,4) for c in range(4)), GRB.GREATER_EQUAL, quicksum(x[0, index, a, b, c]*a for a in range(1,4) for b in range(1,4) for c in range(4)))
+            model.addConstr(quicksum(x[1, index, a, b, c]*a for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.GREATER_EQUAL, quicksum(x[0, index, a, b, c]*a for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)))
+            model.addConstr(quicksum(x[1, index, a, b, c]*b for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)), GRB.GREATER_EQUAL, quicksum(x[0, index, a, b, c]*a for a in range(1, n_trains) for b in range(1, n_trains) for c in range(n_trains)))
            
 
 
@@ -122,9 +123,9 @@ print('Set all constraints', time.time() - startTimeSetUp)
 model.optimize()
 
 for index, row in r_ret.iterrows():
-    for a in range(1,4):
-        for b in range(1,4):
-            for c in range(4):
+    for a in range(1, n_trains):
+        for b in range(1, n_trains):
+            for c in range(n_trains):
                 if x[1, index, a, b, c].X > 0:
                     print([1, index, a,b,c])
                 if x[0, index, a, b, c].X >0:
